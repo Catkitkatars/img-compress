85af154d684370581e9478223935f4be5ba9c5a7
peseniy <catkitkat.ars@yandex.ru>
2025-06-24 13:40:10 +0500

    logger remove

--- cmd/img-compress/main.go
+++ cmd/img-compress/main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"img-compress/internal/config"
 	srv "img-compress/internal/http"
-	"img-compress/internal/lib/logger/slog"
 	"img-compress/internal/storage/sqlite"
 	"log/slog"
 	"os"
 )
 
+const (
+	EnvLocal = "local"
+	EnvDev   = "dev"
+	EnvProd  = "prod"
+)
+
 func main() {
 	cfg := config.Init()
 
-	log := sl.New(cfg.Env)
+	var handler slog.Handler
+
+	switch cfg.Env {
+	case EnvLocal:
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
+	case EnvDev:
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
+	case EnvProd:
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})
+	}
+
+	log := slog.New(handler)
 
 	log.Info("starting img-compress", slog.String("env", cfg.Env))
 
